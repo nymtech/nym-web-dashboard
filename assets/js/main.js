d98cb4eaf2d7d8d9e305bba35e771a8d1c5a7b2d
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-09-26 11:31:42 +0100

    Let's try a simple socket connection

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,40 @@ function updateCocoNodes(cocoNodes) {
   });
 }
 
+function connectWebSocket () {
+  var conn;
+  var msg = document.getElementById("msg");
+  var log = document.getElementById("log");
+
+  function appendLog(item) {
+    var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
+    log.appendChild(item);
+    if (doScroll) {
+      log.scrollTop = log.scrollHeight - log.clientHeight;
+    }
+  }
+
+  console.log("connecting to websocket");
+  conn = new WebSocket("wss://directory.nymtech.net/ws");
+  conn.onopen = function(evt) {
+    console.log("Received evt: " + evt);
+  };
+  conn.onclose = function(evt) {
+    var item = document.createElement("div");
+    item.innerHTML = "<b>Connection closed.</b>";
+    appendLog(item);
+  };
+  conn.onmessage = function(evt) {
+    var messages = evt.data.split('\n');
+    for (var i = 0; i < messages.length; i++) {
+      var item = document.createElement("div");
+      item.innerText = messages[i];
+      appendLog(item);
+    }
+  };
+}
+
 $(document).ready(function() {
   getTopology();
+  connectWebSocket();
 });
